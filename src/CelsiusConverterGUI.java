d580226f996dc4f1371e9c878f04473ed532b56e
April Howard <aprilhoward@Aprils-MacBook.fios-router.home>
2020-05-24 01:36:59 -0400

    created GUI form, JFrame, title for program, visible set to true

---
+++ src/CelsiusConverterGUI.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class CelsiusConverterGUI extends JFrame{
+    private JPanel mainPanel;
+
+    public CelsiusConverterGUI(String title) {
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new CelsiusConverterGUI("Celsius Converter");
+        frame.setVisible(true);
+    }
+}
