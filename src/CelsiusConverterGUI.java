e5d4392c70fb8ecf4830b5c192dc1c1239bbfc7b
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-05-24 19:02:22 -0400

    created ActionListener to grab text from Celsius input, convert it and display Fahrenheit

--- src/CelsiusConverterGUI.java
+++ src/CelsiusConverterGUI.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
-// https://www.youtube.com/watch?v=-SmNpKskfJc
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class CelsiusConverterGUI extends JFrame{
     private JPanel mainPanel;
     private JButton Convert;
-    private JTextField textField1;
+    private JTextField celsiusTextField;
     private JLabel Celsius;
     private JLabel Fahrenheit;
 
@@ -14,6 +16,18 @@ public class CelsiusConverterGUI extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+        Convert.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // grab the text from the CelsiusTextField
+                // convert to a double
+                // update the FahrenheitLabel
+                // parse degrees celsius
+                int tempFahr = (int)((Double.parseDouble(celsiusTextField.getText()))
+                * 1.8 + 12);
+                Fahrenheit.setText(tempFahr + " Fahrenheit");
+            }
+        });
     }
 
     public static void main(String[] args) {
